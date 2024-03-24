f8c0217ae01826418493ed51296d54246c84554f
Steven Getty <nonstopcoding@gmail.com>
2024-03-24 00:16:24 +0000

    Update functions.php
    added logs function plus 2 functions to validate urls

--- includes/functions.php
+++ includes/functions.php
@@ -17,12 +17,12 @@ function getUserName($user = 0) {
 		return false;
 	}
 	# Since the data input is a number as expected we can scan the database for the username of the id inputed
-	$db->query('SELECT username FROM members WHERE id = ?');
+	$db->query('SELECT usr_username FROM members WHERE usr_id = ?');
 	$db->execute([$user]);
 	# If the user exists it returns the data with some protection included just incase some bad code gets in otherwise function does nothing
 	if ($db->count()) {
 		$info = $db->fetch();
-		return htmlentities(stripslashes($info['username']));
+		return htmlentities(stripslashes($info['usr_username']));
 	}
 }
 
@@ -40,4 +40,29 @@ function showItems($id = 0) {
 			# List items and item type
 		}
 	}
+}
+
+function createLog($logtype = '``', $user = 0, $page = $_SERVER['PHP_SELF']) {
+	global $db;
+	$ip = $_SERVER['REMOTE_ADDR'];
+	$ipbackup = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	$logtype = (!empty($logtype) && strlen($logtype) < 255) ? $logtype : false;
+	$user = abs(intval($user));
+	$page = full_url($page);
+	$text = addslashes(htmlentities($text));
+}
+
+function url_origin($s, $use_forwarded_host = false ) {
+    $ssl = ( ! empty( $s['HTTPS'] ) && $s['HTTPS'] == 'on' );
+    $sp = strtolower( $s['SERVER_PROTOCOL'] );
+    $protocol = substr( $sp, 0, strpos( $sp, '/' ) ) . ( ( $ssl ) ? 's' : '' );
+    $port = $s['SERVER_PORT'];
+    $port = ( ( ! $ssl && $port=='80' ) || ( $ssl && $port=='443' ) ) ? '' : ':'.$port;
+    $host = ( $use_forwarded_host && isset( $s['HTTP_X_FORWARDED_HOST'] ) ) ? $s['HTTP_X_FORWARDED_HOST'] : ( isset( $s['HTTP_HOST'] ) ? $s['HTTP_HOST'] : null );
+    $host = isset( $host ) ? $host : $s['SERVER_NAME'] . $port;
+    return $protocol . '://' . $host;
+}
+
+function full_url( $s, $use_forwarded_host = false ) {
+    return url_origin($s, $use_forwarded_host ).$s['REQUEST_URI'];
 }
